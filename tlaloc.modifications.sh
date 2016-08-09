ec7f15a4392d5b0acc16d00f3e74c2bb52a4a10b
Timo Pallach <timo@pallach.de>
2016-08-09 16:05:30 +0200

    The tlaloc.modifications.sh was added to version control because it calls the function to do modifications on the base system.

---
+++ tlaloc.modifications.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# tlaloc modifications script
+# Copyright Timo Pallach <timo@pallach.de>, see LICENSE for details
+
+do_modifications() {
+  debug "DEBUG" "No modifications are done here."
+}
